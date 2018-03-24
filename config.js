f7f3977d710637e99ae984afca0daa767a8d81b8
Mettiu <mettiu@gmail.com>
2018-03-24 19:48:12 +0100

    Some small adjustments to project starting point.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const configPath = './config.json';
+const configPath = `${__dirname}/config.json`;
 const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 
 // We have to export each object in order to access them separately
