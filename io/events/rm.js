2ffc45b1f1f638c542e38243c8d4e128763c7f83
Mettiu <mettiu@gmail.com>
2018-04-01 21:05:13 +0200

    closes #15

---
+++ io/events/rm.js
@@ -0,0 +1,24 @@
+const File = require('../../models/file');
+
+const mongoose = require('mongoose');
+// configure mongoose promises
+mongoose.Promise = global.Promise;
+
+// manage file remove
+function rm(io) {
+  io.on('connection', (socket) => {
+    socket.on('rm', async (data, fn) => {
+      console.log('data: ', data);
+      console.log('user', socket.sDecoded._id, 'currentfolder', socket.sCurrentFolder);
+
+      const result =
+          await File.deleteFile(socket.sDecoded._id, socket.sCurrentFolder._id, data.fileName);
+      fn(JSON.stringify({
+        error: false,
+        deleted: result,
+      }));
+    });
+  });
+}
+
+module.exports = rm;
